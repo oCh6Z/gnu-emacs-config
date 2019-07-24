9542eeb754a4f8b88316cc34eca36a74108b177e
och6z <44880194+och6z@users.noreply.github.com>
2019-07-25 01:29:07 +0200

    Update indent config

--- .emacs.d/configurations/general-config.el
+++ .emacs.d/configurations/general-config.el
@@ -3,5 +3,16 @@
 	(tool-bar-mode 0)
   (scroll-all-mode t))
 
+;;;;;;;;;;;;;
+;; Editing ;;
+;;;;;;;;;;;;;
+
+(use-package indent
+			 :config
+			 ;; control tabs or space are used for indentation
+			 (setq-default indent-tabs-mode t)
+			 ;; list of tab stop positions
+			 (setq tab-stop-list (number-sequence 4 200 4)))
+
 ;; add the symbol name to the features list
 (provide 'general-config)
