c4aae2f93a5d1b6e2614ecd877238cf334fecc01
och6z <44880194+och6z@users.noreply.github.com>
2019-07-25 02:23:21 +0200

    Update setq variables

--- .emacs.d/configurations/general-config.el
+++ .emacs.d/configurations/general-config.el
@@ -8,5 +8,25 @@
 			  truncate-lines t
 			  indent-tabs-mode t)
 
+(setq tab-stop-list (number-sequence 4 200 4)
+	  time-stamp-pattern '("8/Last modified:[ \t]+\\\\?[\"<]+%:y-%02m-%02d %02H:%02M:%02S\\\\?[\">]")
+	  backup-by-copying t
+	  version-control t
+	  delete-old-versions t
+	  kept-old-versions 1
+	  kept-new-versions 5
+	  backup-directory-alist '(("" . "~/.emacs.d/backup"))
+	  vc-make-backup-files t
+	  auto-save-default nil
+	  ring-bell-function #'ignore
+	  inhibit-startup-screen t
+	  initial-scratch-message nil
+	  blink-cursor-mode t
+	  line-number-mode t
+	  column-number-mode t
+	  font-use-system-font nil
+	  browse-url-browser-function 'browse-url-generic
+	  browse-url-generic-program "chromium")
+
 ;; add the symbol name to the features list
 (provide 'general-config)
