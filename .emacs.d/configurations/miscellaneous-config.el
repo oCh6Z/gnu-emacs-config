5ede348921248e8c437162eda15d343e65b4154a
jrajm@undtest <jrajm@undtest>
2019-07-29 01:54:09 +0200

    Add company package

--- .emacs.d/configurations/miscellaneous-config.el
+++ .emacs.d/configurations/miscellaneous-config.el
@@ -2,5 +2,19 @@
 ;; Miscellaneous configurations ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package company
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (setq company-tooltip-align-annotations t
+		company-idle-delay 0.2
+		company-minimum-prefix-length 2
+		company-require-match nil
+		company-dabbrev-ignore-case nil
+		company-dabbrev-code-ignore-case nil
+		company-dabbrev-downcase nil))
+
 ;; add the symbol name to the features list
 (provide 'miscellaneous-config)
