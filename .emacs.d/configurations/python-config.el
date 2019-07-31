4361edabecd5f93cc9d946b4b9db6be844aba45d
och6z <44880194+och6z@users.noreply.github.com>
2019-07-31 14:45:59 +0200

    Add python package

--- .emacs.d/configurations/python-config.el
+++ .emacs.d/configurations/python-config.el
@@ -2,5 +2,19 @@
 ;; Python configurations ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package python
+  :ensure nill
+  :mode
+  ("\\.py$" . python-mode)
+  :config
+  (setq python-shell-interpreter "python"
+		python-indent-offset 4
+		python-indent-guess-indent-offset t
+		python-indent-guess-indent-offset-verbose nil
+		;; iPython interpreter
+		; python-shell-interpreter "ipython"
+		; python-shell-interpreter-args "--simple-prompt -i"
+		))
+
 ;; add the symbol name to the features list
 (provide 'python-config)
